d4553418315c692846fc8fb75f5e4de91a1eb408
Thomas Curtis <thomascurtis2001@icloud.com>
2025-05-18 16:17:52 -0400

    Add CORS middleware to Express server and update dependencies

--- lib/server.ts
+++ lib/server.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors";
 import weatherRoutes from "./routes/tomorrowioRoutes";
 
 const app = express();
 const port = process.env.PORT;
+app.use(cors());
 
 app.use('/weather', weatherRoutes);
 app.listen(port, () => {
