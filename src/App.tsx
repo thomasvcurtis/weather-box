a3c42cf94ebe951c55249696eaffbefe082c7a1e
Thomas Curtis <thomascurtis2001@icloud.com>
2025-05-21 21:50:52 -0400

    feat: integrate Mapbox GL and update app layout
    - Added Mapbox GL as a dependency for map rendering.
    - Refactored App component to initialize and manage Mapbox map instance.
    - Updated CSS styles to accommodate full-screen map and sidebar for coordinates display.
    - Removed unused assets and styles to streamline the project.
    - Added TypeScript definitions for environment variables to support Mapbox API key.

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useRef, useEffect, useState } from 'react'
+import mapboxgl, { Map, LngLatLike } from 'mapbox-gl'
+
+import 'mapbox-gl/dist/mapbox-gl.css';
 import './App.css'
 
+const INITIAL_CENTER: [number, number] = [
+  -74.0242,
+  40.6941
+]
+const INITIAL_ZOOM = 10.12
+
 function App() {
-  const [count, setCount] = useState(0)
+  const mapRef = useRef<Map | null>(null)
+  const mapContainerRef = useRef<HTMLDivElement>(null)
+
+  const [center, setCenter] = useState<[number, number]>(INITIAL_CENTER)
+  const [zoom, setZoom] = useState<number>(INITIAL_ZOOM)
+
+  useEffect(() => {
+    if (!mapContainerRef.current) return;
+
+    mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_API_KEY
+    mapRef.current = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      center: center as LngLatLike,
+      zoom: zoom
+    });
+
+    mapRef.current.on('move', () => {
+      if (!mapRef.current) return;
+      const mapCenter = mapRef.current.getCenter()
+      const mapZoom = mapRef.current.getZoom()
+
+      setCenter([mapCenter.lng, mapCenter.lat])
+      setZoom(mapZoom)
+    })
+
+    return () => {
+      mapRef.current?.remove()
+    }
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="sidebar">
+        Longitude: {center[0].toFixed(4)} | Latitude: {center[1].toFixed(4)} | Zoom: {zoom.toFixed(2)}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div id='map-container' ref={mapContainerRef} />
     </>
   )
 }
