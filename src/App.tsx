e0a7636410177f1bc480dd1ca9696bf2d431f29d
Thomas Curtis <thomascurtis2001@icloud.com>
2025-05-21 22:14:33 -0400

    feat: restructure app to include MapBox component and update README and index.html

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,8 @@
-import { useRef, useEffect, useState } from 'react'
-import mapboxgl, { Map, LngLatLike } from 'mapbox-gl'
-
-import 'mapbox-gl/dist/mapbox-gl.css';
-import './App.css'
-
-const INITIAL_CENTER: [number, number] = [
-  -74.0242,
-  40.6941
-]
-const INITIAL_ZOOM = 10.12
+import MapBox from "./components/Map";
+import "./App.css";
 
 function App() {
-  const mapRef = useRef<Map | null>(null)
-  const mapContainerRef = useRef<HTMLDivElement>(null)
-
-  const [center, setCenter] = useState<[number, number]>(INITIAL_CENTER)
-  const [zoom, setZoom] = useState<number>(INITIAL_ZOOM)
-
-  useEffect(() => {
-    if (!mapContainerRef.current) return;
-
-    mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_API_KEY
-    mapRef.current = new mapboxgl.Map({
-      container: mapContainerRef.current,
-      center: center as LngLatLike,
-      zoom: zoom
-    });
-
-    mapRef.current.on('move', () => {
-      if (!mapRef.current) return;
-      const mapCenter = mapRef.current.getCenter()
-      const mapZoom = mapRef.current.getZoom()
-
-      setCenter([mapCenter.lng, mapCenter.lat])
-      setZoom(mapZoom)
-    })
-
-    return () => {
-      mapRef.current?.remove()
-    }
-  }, [])
-
-  return (
-    <>
-      <div className="sidebar">
-        Longitude: {center[0].toFixed(4)} | Latitude: {center[1].toFixed(4)} | Zoom: {zoom.toFixed(2)}
-      </div>
-      <div id='map-container' ref={mapContainerRef} />
-    </>
-  )
+  return <MapBox />;
 }
 
-export default App
+export default App;
---
+++ src/components/Map.tsx
@@ -0,0 +1,47 @@
+import { useRef, useEffect, useState } from "react";
+import mapboxgl, { Map, LngLatLike } from "mapbox-gl";
+
+
+const INITIAL_CENTER: [number, number] = [-83.6448, 41.3703];
+const INITIAL_ZOOM = 10.12;
+
+export default function MapBox() {
+  const mapRef = useRef<Map | null>(null);
+  const mapContainerRef = useRef<HTMLDivElement>(null);
+
+  const [center, setCenter] = useState<[number, number]>(INITIAL_CENTER);
+  const [zoom, setZoom] = useState<number>(INITIAL_ZOOM);
+
+  useEffect(() => {
+    if (!mapContainerRef.current) return;
+
+    mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_API_KEY;
+    mapRef.current = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      center: center as LngLatLike,
+      zoom: zoom,
+    });
+
+    mapRef.current.on("move", () => {
+      if (!mapRef.current) return;
+      const mapCenter = mapRef.current.getCenter();
+      const mapZoom = mapRef.current.getZoom();
+
+      setCenter([mapCenter.lng, mapCenter.lat]);
+      setZoom(mapZoom);
+    });
+
+    return () => {
+      mapRef.current?.remove();
+    };
+  }, []);
+  return (
+    <>
+      <div className="sidebar">
+        Longitude: {center[0].toFixed(4)} | Latitude: {center[1].toFixed(4)} |
+        Zoom: {zoom.toFixed(2)}
+      </div>
+      <div id="map-container" ref={mapContainerRef} />
+    </>
+  );
+}
